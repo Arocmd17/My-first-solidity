b36bf33f8f2d132c12a3eba1bb5f2bad9c5a3039
"Ruben Olakanmi Akande" <"akanderuben17@gmail.com">
2022-03-24 17:56:27 +0100

    added a contract to deploy contract

---
+++ contract/StorageFactory.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+import "./SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage[] public simpleStorageArray;
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+}
