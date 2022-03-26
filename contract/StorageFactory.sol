c9a359ce8f82466371a299d83636a526ad8cddac
"Ruben Olakanmi Akande" <"akanderuben17@gmail.com">
2022-03-26 19:21:59 +0100

    inheritance added

--- contract/StorageFactory.sol
+++ contract/StorageFactory.sol
@@ -1,11 +1,26 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
+
+//This contract deploy contract
 import "./SimpleStorage.sol";
 
-contract StorageFactory {
+contract StorageFactory is SimpleStorage {
     SimpleStorage[] public simpleStorageArray;
     function createSimpleStorageContract() public {
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArray.push(simpleStorage);
+    }
+
+    //Locate a contract in the contract Array the call the "store" function the SimpleStorage contract
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        //Address
+        //ABI
+        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        simpleStorage.store(_simpleStorageNumber);
+    }
+    //Locate a contract in the contract Array the call the "retrieve" function the SimpleStorage contract
+    function sfGet(uint _simpleStorageIndex) public view returns (uint256) {
+        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        return simpleStorage.retrieve();
     }
 }
